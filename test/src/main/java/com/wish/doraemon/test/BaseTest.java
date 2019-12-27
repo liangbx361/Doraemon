f7bf3f9d39bd56f15b55f2c6f386a5aed8d3bb80
liang baoxian <liangbx361@163.com>
2019-12-27 17:17:20 +0800

    提取公共测试模块

---
+++ test/src/main/java/com/wish/doraemon/test/BaseTest.java
@@ -0,0 +1,11 @@
+package com.wish.doraemon.test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {TestApplication.class})
+public class BaseTest {
+
+}
