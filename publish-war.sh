1927aabbd610030717ba114e5028f794edb85911
liang baoxian <liangbx361@163.com>
2020-01-20 10:07:38 +0800

    分离生产和开发环境的配置；更新发布脚本

--- publish-war.sh
+++ publish-war.sh
@@ -1,3 +1,4 @@
 cd /Users/lbx/dev/server/project/doraemon
 ./gradlew bootWar
-scp -P 22 ./build/libs/doraemon-0.0.1-SNAPSHOT.war root@49.235.52.159:/opt/apache-tomcat-8.5.45/webapps/doraemon.war
+mv ./app/build/libs/app-0.0.1-SNAPSHOT.war ./app/build/libs/doraemon-0.0.1-SNAPSHOT.war
+scp -P 22 ./app/build/libs/doraemon-0.0.1-SNAPSHOT.war root@49.235.52.159:/opt/apache-tomcat-8.5.45/webapps/doraemon.war
